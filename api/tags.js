236efb485bfeb1fcd55da936af450f4e319ab85c
tkesterson <travis.kesterson@gmail.com>
2021-04-19 21:09:08 -0400

    fix

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const tagsRouter = express.Router();
-const { getAllTagss } = require("../db");
+const { getAllTags } = require("../db");
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
